487a01caf89488d7b3169f7efa2e6fffbab7f99c
miolex <mioleh666@gmail.com>
2021-06-13 16:38:20 +0300

    .

---
+++ src/testclasses/TestBanking.java
@@ -0,0 +1,14 @@
+package testclasses;
+
+/**
+ *
+ * @author taurus
+ */
+public class TestBanking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { }
+    
+}
